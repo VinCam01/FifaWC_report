d48d56ac6e32def862b2aecf0cbd3aef5e93454a
VinCam01 <151729630+VinCam01@users.noreply.github.com>
2023-11-23 22:39:44 +0100

    Update Python_Project.py

--- Python_Project.py
+++ Python_Project.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
+import re
 
 pd.set_option('display.max_columns',200)
 
@@ -30,18 +32,22 @@ print(wcups.describe().T.round(2))
 
 #%% GOAL SCORED FOR EACH WORLD CUP
 plt.plot(wcups['Year'], wcups['GoalsScored'])
+plt.xlabel('World Cup')
+plt.ylabel('Number of goal')
 plt.xticks(wcups['Year'], rotation = 'vertical')
 plt.yticks(range(70, 175, 10))
 plt.grid()
-plt.title('GOAL SCORED FOR EACH WORLD CUP')
+plt.title('Goal Scored For Each World Cup')
 plt.show()
 
-#%% COUNTRY THAT WON THE MOST WORLD CUPS
+#%% COUNTRIES THAT WON MOST WORLD CUPS
 wcups = wcups.replace('Germany FR', 'Germany')
 plt.hist(wcups['Winner'],bins=15,edgecolor="black")
+plt.xlabel('Country')
+plt.ylabel('Number of wins')
 plt.xticks(wcups['Winner'], rotation = 'vertical')
 plt.yticks(range(0, 10, 1))
-plt.title('Who won most World Cups?')
+plt.title('Who Won Most World Cups?')
 plt.grid(True)
 plt.show()
 
@@ -52,31 +58,33 @@ players = pd.merge(players, matches[['MatchID', 'Year']], on='MatchID', how='lef
 x = players[players['Year']>1950]
 
 plt.hist(x['Shirt Number'], bins = 50)
+plt.xlabel('Shirt Number')
+plt.ylabel('Number of times it has been used')
 plt.xticks(players['Shirt Number'].unique())
 plt.grid()
-plt.title('MOST USED SHIRT NUMBERS FROM 1954 TO 2014')
+plt.title('Most Used Shirt Numbers From 1954 To 2014')
 comment = 'Although we see very particular numbers in national leagues, \nFIFA regulations state: "The shirt numbers of players must \ncorrespond to those indicated in the official team list (1 to 23)." \nSince Qatar 2022, the list is extended to 26 player'
+plt.text(1.02, 0.5, comment, ha='left', va='center', transform=plt.gcf().transFigure, fontsize=10)
 plt.show()
 
-#CONTO QUANTI REFEREE CI SONO PER OGNI NAZIONE
-x['Shirt Number'].value_counts()
-
-#ESTRAGGO SOLO LA SIGLA DEL REFEREE
+#%% HOW MANY MATCHES HAVE REFEREES OF EACH NATIONALITY DIRECTED?
+'''
+Referee column is structured in this form : Name Surname (NAT)
+Since we just need their nationality, we modify the column in order to have just NAT
+'''
 matches['Referee'] = matches['Referee'].str.extract(r'\((.*?)\)')
-#type(matches['Referee'])
-matches
-
-#CONTO QUANTI REFEREE CI SONO PER OGNI NAZIONE
 ref_nationality = matches['Referee'].value_counts()
 ref_nationality
 
-#vediamo quali sono le nazionalità degli arbitri più frequenti, escludendo quelle minori di 5
-ref_nationality = ref_nationality[ref_nationality>5]
+ref_nationality = ref_nationality[ref_nationality>5]   #We only show values greater than 5 in the graph to make it more readable 
 plt.bar(ref_nationality.index, ref_nationality)
+plt.xlabel('Referee Nationality')
+plt.ylabel('Number of matches')
 plt.xticks(rotation=90, ha='right', fontsize=5)
+plt.title('How Many Matches Have Referees Of Each Nationality Directed?')
 plt.show()
 
-#Total number of goals scored in games refereed by a referee from a particular nation
+#%% Total number of goals scored in games directed by a referee from a particular nation
 dic = {}
 for i in range(len(matches['Referee'])):
   if matches['Referee'][i] in ref_nationality:
@@ -86,29 +94,30 @@ for i in range(len(matches['Referee'])):
       dic[matches['Referee'][i]] = dic[matches['Referee'][i]] + matches['Home Team Goals'][i] + matches['Away Team Goals'][i]
 
 plt.bar(dic.keys(), dic.values())
-
-plt.xlabel('Referee')
+plt.xlabel('Referee Nationality')
 plt.ylabel('Goal Scored ')
-plt.title('Total number of goals scored in games refereed by a referee from a particular nation ')
+plt.title('Total Number Of Goals Scored In Games Directed By A Referee From A Particular Nation')
 plt.grid()
 plt.xticks(rotation=90, ha='right', fontsize=5)
 plt.show()
 
-#Mean of Goal by ref nationality
+#%% Mean of goal scored in games directed by a referee from a particular nation
 mean_gol_by_ref_nat = {}
 for i in dic.keys():
   mean_gol_by_ref_nat[i] = (dic[i] / ref_nationality[i]).round(2)
 
 plt.bar(mean_gol_by_ref_nat.keys(), mean_gol_by_ref_nat.values())
-
-plt.xlabel('Referee')
+plt.xlabel('Referee Nationality')
 plt.ylabel('Goal per match')
-plt.title('Mean of Goal by ref nationality')
+plt.title('Mean Of Goal Scored In Games Directed By A Referee From A Particular Nation')
 plt.grid()
 plt.xticks(rotation=90, ha='right', fontsize=5)
 plt.show()
 
-#ADD THE COLUMN Yellow or Red IN THE DF PLAYERS
+#%% CREATE THE COLUMN 'Yellow or Red' IN THE PLAYERS DATAFRAME
+'''
+For each player the value will be 1 if he received a cards, 0 otherwise
+'''
 lst = []
 for i in players['Event']:
   if type(i) == str:
@@ -122,9 +131,10 @@ players['Yellow or Red'] = lst
 
 players['Yellow or Red'].value_counts()
 
-players
-
-#CREO UNA COLONNA NEL DATAFRAME MATCHES COL NUMERO DI CARTELLINI DELLA PARTITA
+#%% CREATE THE COLUMN 'Cards' IN THE MATCHES DATAFRAME
+'''
+For each match, this value represent the number of yellow or red card given by the referee
+'''
 card_per_match = {}
 for i in range(len(players['MatchID'])):
   if players['MatchID'][i] not in card_per_match:
@@ -133,9 +143,9 @@ for i in range(len(players['MatchID'])):
     card_per_match[players['MatchID'][i]] = card_per_match[players['MatchID'][i]] + players['Yellow or Red'][i]
 
 matches['Cards'] = matches['MatchID'].map(card_per_match)
-matches
+print(matches)
 
-# QUANTI CARTELLINI DANNO GLI ARBITRI DI UNA DATA NAZIONALITA'?
+#%% TOTAL NUMBER OF CARDS GIVEN BY REFEREES OF A GIVEN NATIONALITY
 dic = {}
 for i in range(len(matches['Referee'])):
   if matches['Referee'][i] in ref_nationality:
@@ -145,37 +155,36 @@ for i in range(len(matches['Referee'])):
       dic[matches['Referee'][i]] = dic[matches['Referee'][i]] + matches['Cards'][i]
 
 plt.bar(dic.keys(), dic.values())
-
-plt.xlabel('Referee')
-plt.ylabel('Cards ')
-plt.title('Total number of cards given by ref of a given nationality ')
+plt.xlabel('Referee Nationality')
+plt.ylabel('Cards')
+plt.title('Total Number Of Cards Given By Referees Of A Given Nationality ')
 plt.grid()
 plt.xticks(rotation=90, ha='right', fontsize=5)
 plt.show()
 
-# QUANTI CARTELLINI DANNO GLI ARBITRI DI UNA DATA NAZIONALITA' PER PARTITA?
+#%% MEAN OF CARDS GIVEN BY REFEREES OF A GIVEN NATIONALITY
 mean_cards_by_ref_nat = {}
 for i in dic.keys():
   mean_cards_by_ref_nat[i] = (dic[i] / ref_nationality[i]).round(2)
 
 plt.bar(mean_cards_by_ref_nat.keys(), mean_cards_by_ref_nat.values())
-
-plt.xlabel('Referee')
+plt.xlabel('Referee Nationality')
 plt.ylabel('Cards per match')
-plt.title('Cards of Goal by ref nationality')
+plt.title('Which Are The Strictest Referees?')
 plt.grid()
 plt.xticks(rotation=90, ha='right', fontsize=5)
 plt.show()
 
-#HOW MANY GOAL HAS BEEN SCORED IN THESE SLOTS?
-import re
+#%% HOW MANY GOAL HAS BEEN SCORED IN THESE SLOTS?
+'''
+SLOT:  1-22 / 23-45 / 46-67 / 68-90
+'''
 def estrai_minuti_gol(a):
     minuti_gol = re.findall(r'G(\d+)', str(a))
     return [int(x) for x in minuti_gol]
 
 players['Minuti_gol'] = players['Event'].apply(estrai_minuti_gol)
 
-#SLOT 1-22 / 23-45 / 46-67 / 68-90
 goal_scored_in_1_22=0
 goal_scored_in_23_45=0
 goal_scored_in_46_67=0
@@ -193,14 +202,14 @@ for i in players['Minuti_gol']:
       elif 68 <= minute <= 90:
         goal_scored_in_68_90 += 1
 
-#print(goal_scored_in_1_22, goal_scored_in_23_45, goal_scored_in_46_67, goal_scored_in_68_90)
-slot={'1/22':goal_scored_in_1_22, '23/45':goal_scored_in_23_45, '46/67':goal_scored_in_46_67,'68/90':goal_scored_in_68_90}
+slot={'1-22':goal_scored_in_1_22, '23-45':goal_scored_in_23_45, '46-67':goal_scored_in_46_67,'68-90':goal_scored_in_68_90}
 plt.bar(slot.keys(), slot.values())
-plt.title('Gol in time slots')
+plt.xlabel('Time slot')
+plt.ylabel('Goals scored')
+plt.title('Number Of Goals Scored In Different Time Slots')
 plt.show()
 
-#HOW MANY TIMES ITALY FINISH 1-2-3-4?
-wcups
+#%% HOW MANY TIMES ITALY FINISH 1-2-3-4?
 winner = 0
 runner_up = 0
 third = 0
@@ -222,9 +231,12 @@ for i in wcups['Fourth']:
 italy = {'1 Place': winner, '2 Place': runner_up, '3 Place': third, '4 Place': fourth}
 
 plt.pie(list(italy.values()), labels=list(italy.keys()), autopct='%1.1f%%')
+plt.title("How Will Italy's World Cup End If They Get Past The Quarter-Finals?")
+comment = '1 Place: ' + str(winner) + '\n2 Place: ' + str(runner_up) + '\n3 Place: ' + str(third) + '\n4 Place: ' + str(fourth)
+plt.text(1.02, 0.5, comment, ha='left', va='center', transform=plt.gcf().transFigure, fontsize=10)
 plt.show()
 
-# which are the most common combinations of first/second half result
+#%% CREATE THE COLUMNS 'Winner Match' and 'Winner Half Time'
 winner=[]
 first_half=[]
 for i in range(len(matches)):
@@ -242,10 +254,12 @@ for i in range(len(matches)):
     first_half.append('Draw')
 
 matches['Winner Match'] = winner
-matches['winner Half Time'] = first_half
+matches['Winner Half Time'] = first_half
 
-#11 1D 12 DD D1 D2 21 2D 22
-
+#%% Which are the most common combinations of first/second half result?
+'''
+Possible combinations: 11 1X 12 XX X1 X2 21 2X 22
+'''
 First_First = 0
 First_Draw = 0
 First_Second = 0
@@ -256,14 +270,14 @@ Second_First = 0
 Second_Draw = 0
 Second_Second = 0
 for i in range(len(matches)):
-  if matches['winner Half Time'][i] == 'First':
+  if matches['Winner Half Time'][i] == 'First':
     if matches['Winner Match'][i] == 'First':
       First_First += 1
     elif matches['Winner Match'][i] == 'Draw':
       First_Draw += 1
     else:
       First_Second += 1
-  elif matches['winner Half Time'][i] == 'Draw':
+  elif matches['Winner Half Time'][i] == 'Draw':
     if matches['Winner Match'][i] == 'First':
      Draw_First += 1
     elif matches['Winner Match'][i] == 'Draw':
@@ -283,27 +297,31 @@ combinations = {'1/1': First_First, '1/X': First_Draw, '1/2': First_Second,
                 '2/1': Second_First, '2/X': Second_Draw, '2/2': Second_Second}
 
 plt.pie(list(combinations.values()), labels=list(combinations.keys()), autopct='%1.1f%%')
+plt.title('First - Second Half Combinations')
 plt.show()
 
-matches.info()
 
-#creo nuova colonna che indica se l'evento 1 tempo è uguale all'evento 2 tempo
+
+#%% CREATE A NEW COLUMN '1Half = 2Half' IN THE MATCHES DATAFRAME
+'''
+the value will be 1 if the result of first half will be the same of the second, 0 otherwise
+'''
 first_second=[]
 for i in range(len(matches)):
-  if matches['winner Half Time'][i] == matches['Winner Match'][i]:
+  if matches['Winner Half Time'][i] == matches['Winner Match'][i]:
     first_second.append(1)
   else:
     first_second.append(0)
-matches['first half outcome = second half outcome'] = first_second
+matches['1Half = 2Half'] = first_second
 matches
 
-#correlazione
-import seaborn as sns
+#%% CORRELATION
 corr_target_1 = round(matches[['Home Team Goals', 'Away Team Goals',
                                'Attendance', 'Half-time Home Goals',
-                               'Half-time Away Goals','first half outcome = second half outcome',
+                               'Half-time Away Goals','1Half = 2Half',
                                'Cards']].corr(),2)
 
 plt.figure(figsize = (18,6))
+plt.title('Correlation Heatmap')
 sns.heatmap(corr_target_1, annot = True, fmt='.2g',cmap= 'coolwarm')
 plt.show()
